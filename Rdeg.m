d6daf8cee710bace2858779d54b524ceb5610f3f
Ryan Gray <ryangray@comcast.net>
2005-07-29 22:04:04 +0000

    Added Rankine degrees, but they are the same as Kdeg and Cdeg since units does not do scale shifts that would let it support degF, degC, degK and degR.

---
+++ Rdeg.m
@@ -0,0 +1,7 @@
+% Unit Rdeg 
+% Converts number to internal units by multplying by it 
+% Converts number into Rdeg by dividing value in internal units by it. 
+
+function x = unit 
+
+x = Rdegree; 
---
+++ Rdegree.m
@@ -0,0 +1,7 @@
+% Unit Rdegree 
+% Converts number to internal units by multplying by it 
+% Converts number into Kdegree by dividing value in internal units by it. 
+
+function x = unit 
+
+x = Kdegree; 
