4f63574e650550c505cc38e5b1041190de0f378e
Ryan Gray <ryangray@comcast.net>
2005-08-27 08:11:00 +0000

    Copying g0 constant into units. May make constants library separate or keep dupes of them in both units and Vectorlab.

---
+++ g0.m
@@ -0,0 +1,9 @@
+% g0
+% Earth gravitational acceleration standard value
+% Ellipsoidal Earth at 45 degrees latitude
+% = 9.80665 m/s/s or 32.174 ft/s/s
+% Usage: g0
+
+function g0 = constant
+
+g0 = 9.80665 * meter / second / second;
