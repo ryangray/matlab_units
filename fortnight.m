a62d660789503d03a2549cf5f0056042dcf44ac5
Ryan Gray <ryangray@comcast.net>
2006-07-28 17:28:43 +0000

    Added these two important units

---
+++ fortnight.m
@@ -0,0 +1,8 @@
+% Unit fortnight
+% Converts number to internal units by multplying by it 
+% Converts number into mile by dividing value in internal units by it. 
+% See also: DAYS
+
+function fortnight = unit 
+
+fortnight = 14 * days;
---
+++ furlong.m
@@ -0,0 +1,8 @@
+% Unit furlong
+% Converts number to internal units by multplying by it 
+% Converts number into mile by dividing value in internal units by it. 
+% See also: STATUTE_MILE, FEET
+
+function furlong = unit 
+
+furlong = 660 * feet; 
