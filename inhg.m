ae05cdc99b38550e6528434b304a5e08e976e166
Ryan Gray <ryangray@comcast.net>
2006-08-15 20:06:47 +0000

    Made mmhg definition exact and added elated units of inhg and torr

--- inhg.m
+++ inhg.m
@@ -1,8 +1,8 @@
-% Unit mmhg (millimeters of Mercury)
+% Unit inhg = inches of mercury
 % Converts number to internal units by multplying by it 
 % Converts number into mmhg by dividing value in internal units by it. 
-% See also: KPA, PA, ATM
+% See also: MMHG
 
-function mmhg = unit 
+function inhg = unit 
 
-mmhg =  101325/760 * pa; % 10th CGPM, 1954
+inhg = inch/mm * mmhg; % 
--- torr.m
+++ torr.m
@@ -1,8 +1,8 @@
-% Unit mmhg (millimeters of Mercury)
+% Unit torr = mmhg
 % Converts number to internal units by multplying by it 
 % Converts number into mmhg by dividing value in internal units by it. 
-% See also: KPA, PA, ATM
+% See also: MMHG
 
-function mmhg = unit 
+function torr = unit 
 
-mmhg =  101325/760 * pa; % 10th CGPM, 1954
+torr = mmhg; % 10th CGPM, 1954
