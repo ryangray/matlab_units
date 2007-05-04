d7091fd93dad80d2ec20efcc89cc2eab87ac43a3
Ryan Gray <ryangray@comcast.net>
2007-05-04 01:13:43 +0000

    Added psi unit as psia since psi is a Matlab toolbox function already. psia means psi absolute, which is gauge pressure plus local atmospheric pressure.

---
+++ psia.m
@@ -0,0 +1,8 @@
+% Unit psia = pounds force per square inch (absolute)
+% Converts number to internal units by multplying by it 
+% Converts number into mmhg by dividing value in internal units by it. 
+% See also: LBF, INCH, PASCAL, TORR
+
+function psia = unit
+
+psia = lbf / inch^2;
