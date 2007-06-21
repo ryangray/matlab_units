6e33ae19a6cddc79d17438c133a4f2cae8e1e84b
Ryan Gray <ryangray@comcast.net>
2007-06-21 16:40:32 +0000

    New unit

---
+++ fathom.m
@@ -0,0 +1,8 @@
+% Unit fathom
+% Converts number to internal units by multplying by it 
+% Converts number into mile by dividing value in internal units by it. 
+% See also: YARD
+
+function fathom = unit
+
+fathom = 2 * yard;
