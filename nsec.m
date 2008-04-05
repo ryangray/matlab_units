85784a523d297d7980ddcf527e5b07f9df9109a3
Ryan Gray <ryangray@comcast.net>
2008-04-05 00:39:49 +0000

    Added nsec.m for nanosecond

---
+++ nsec.m
@@ -0,0 +1,8 @@
+% Unit nsec
+% Converts number to internal units by multplying by it 
+% Converts number into seconds by dividing value in internal units by it. 
+% See also: msec, second
+
+function nsec = unit 
+
+nsec = nano*second; 
