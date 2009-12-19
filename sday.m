2ed20605efdb9505fd94826b3c464ba347df4f5d
Ryan Gray <ryangray@comcast.net>
2009-12-19 01:47:21 +0000

    Tweaked sday to one with a reference.

--- sday.m
+++ sday.m
@@ -4,6 +4,10 @@
 
 function sday = unit 
 
-% sday = 23*hours + 56*minutes + 4.09074*seconds; 
+%sday = day / (1 + 1 / 365.2425); % Reference unknown
 
-sday = day / (1 + 1 / 365.2425);
+% Aoki, S., B. Guinot, G. H. Kaplan, H. Kinoshita, D. D. McCarthy and P. K. Seidelmann:
+% "The new definition of Universal Time". Astronomy and Astrophysics 105(2), 359-361, 1982.
+% For J2000 epoch, = 86400 seconds / 1.002737909350795
+
+sday = day / 1.002737909350795;
