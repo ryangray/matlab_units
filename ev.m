92ea68496b60a89e962ca2776fac21fe929afdcd
Ryan Gray <ryangray@comcast.net>
2010-12-22 05:41:05 +0000

    Improved ev value and citation.

--- ev.m
+++ ev.m
@@ -1,10 +1,10 @@
 % Unit ev (electron Volt)
 % Converts number to internal units by multplying by it 
 % Converts number into eV by dividing value in internal units by it. 
-% Note this is the approximate value given by NIST. The real value must be
-% determined by experiment.
-% See also: KEV, JOULE
+% Source: CODATA 2006 via NIST
+% Uncertainty = 0.000 000 040 x 10-19 J
+% See also: kev, joule, constElectronVolt
 
 function ev = unit
 
-ev =  1.602177E-19 * joule;
+ev = 1.602176487e-19 * joule;
