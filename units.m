462099845d605fb92653dcdd3b408231ad841102
Ryan Gray <ryangray@comcast.net>
2014-03-14 16:01:27 +0000

    Major change: fold the SI base into the main units folder. This way, you could not take any subfolders and use just the SI base system. units.m treats the 'si' base specially to handle the paths correctly. To use the unitval object units, you will still need the subfolders 'siobj' and '@unitval'. Also, the object units are available with all bases - toggle with 'units class double' and 'units class object'. You can list the available base systems with 'units system list'. Fix: delay removing units folder from the path when switching unit bases to avoid losing access when an error occurs.

--- units.m
+++ units.m
@@ -1,18 +1,21 @@
 %% Evaluate units expression, show or switch units base, or show help.
 %% Usage
 %  units              % Prints the name of the units system in use
-%  units('base')      % Prints the base unit values in use
-%  units('list')      % Lists all the units m functions
-%  units('help') or units('doc')   % Opens the help document for units
-%  units('system')              % return path to units base system folder
-%  units('system', base_folder) % change units base system
-%  oldsys = units('system', base_folder) % change units base system and
-%                                        % return the previous one 
-%  units install      % Add units folder and SI base folder to the path
+%  units base         % Prints the base unit values in use
+%  units base dimension % Prints the base unit values for dimension given.
+%  units list         % Lists all the units m functions
+%  units help         % Opens the help document for units
+%  units system       % Return path to units base system folder
+%  units system sysname  % Set units system base (si, english, cgs, etc.)
+%  units system list  % List available units system bases
+%  units class        % Show the value class for units in use (double or unitval)
+%  units class double % Set the units data class to double (non-object).
+%  units class object % Set the units data class to unitval (object units).
+%  units install      % Add units folder to the path
 %
 %  units(units_expression) % evaluates the units expression string ignoring
 %                            ignoring local variables and performing some
-%                            special translations like sec -> second.
+%                            special translations like s and sec -> second.
 %
 %  units(units_expression, value)
 %
@@ -31,7 +34,7 @@
 % example: units('degC',0) is equivalent to: degC(0,'to'), converting the
 % temperature of 0 in current units to degC.
 %
-% See also: unitsSymbols, unitsPlot, unit2str
+% See also: unitsSymbols, unitsPlot, unit2str, unitval
 
 function varargout = units (varargin)
 
@@ -70,14 +73,10 @@ elseif ischar(varargin{1})
             
             base = fileparts(mfilename('fullpath'));
             addpath(base,'-END')
-            addpath(fullfile(base,'si'),'-END')
-            fprintf('The pathtool should open with the units and units/si folders added at the end.\n');
+            fprintf('The pathtool should open with the units and folder added at the end.\n');
             fprintf('You should save the new path for future use.\n');
-            fprintf('\nYou should now also click on the MATLAB Start button and choose:\n');
-            fprintf('  Desktop Tools -> View Start Button Configuration Files...\n');
-            fprintf('Then click "Refresh Start Button", then click "Close". This should add or\n');
-            fprintf('update the Units toolbox entry in the help browser. Right now, that just has the\n');
-            fprintf('help document and a list of the units.\n');
+            fprintf('You can use "units system xxxx" to change the units system base if you like.\n');
+            fprintf('You can use "units system list" to see a list of valid ones.\n');
             fprintf('Type: "units help" for the overview documentation.\n');
             fprintf('Some unit functions have other info in their function help text. Try "help lbm".\n');
             pathtool
@@ -124,67 +123,101 @@ elseif ischar(varargin{1})
 
         case 'system'
             
+            % get old base path
+
+            oldpathstr = fileparts(which('unit_SYSTEM'));
+
             if length(varargin) > 1
                 
-                % get old base path
-                
-                oldpathstr = fileparts(which('unit_SYSTEM'));
+                % Get units folder path
 
-                % Check if given system is just a name or a path
+                si = fileparts(which(mfilename));
 
-                pathstr = fileparts(varargin{2});
-
-                if isempty(pathstr)
-
-                    % Assume is a sibling folder of the old units base
-                    % folder
-
-                    pathstr = fileparts(oldpathstr);
-
-                    usePath = fullfile(pathstr,varargin{2});
-
-                else % anything else, user gives a path for it
-
-                    usePath = varargin{2};
-
-                end
-
-                % Verify it at least has an identifier
-
-                if ~exist(fullfile(usePath,'unit_SYSTEM.m'), 'file')
-
-                    if ~exist(usePath,'dir')
-                        if isempty(which('unit_SYSTEM'))
-                            warning('The base unit folder doesn''t appear to be on the path. Run "units setpath" to restore to si first.');
+                if strcmpi(varargin{2},'list')
+                    
+                    % List valid bases
+                    
+                    D = dir(si);
+                    D = D([D.isdir]);
+                    
+                    fprintf('Available unit system bases:\n\n    si\n');
+                    
+                    for id = 1:length(D)
+                        
+                        if D(id).name(1) ~= '.' && exist(fullfile(si,D(id).name,'unit_SYSTEM.m'),'file')
+                            
+                            fprintf('    %s\n', D(id).name);
                         end
-                        error('Units system folder %s does not exist.', usePath);
+                    end
+                    
+                else
+                    
+                    if ~strcmpi(varargin{2},'si')
+
+                        % Check if given system is just a name or a path
+
+                        pathstr = fileparts(varargin{2});
+
+                        if isempty(pathstr)
+
+                            % Assume is a subfolder of the units base folder
+
+                            usePath = fullfile(si,varargin{2});
+
+                        else % anything else, user gives a path for it
+
+                            usePath = varargin{2};
+
+                        end
+
+                        % Verify it at least has an identifier
+
+                        if ~exist(fullfile(usePath,'unit_SYSTEM.m'), 'file')
+
+                            if ~exist(usePath,'dir')
+                                if isempty(which('unit_SYSTEM'))
+                                    warning('The base unit folder doesn''t appear to be on the path. Run "units setpath" to restore to si first.');
+                                end
+                                error('Units system folder %s does not exist.', usePath);
+                            else
+                                error('Units system folder %s does not apear to be valid.', usePath);
+                            end
+
+                        end
+
+                        rmpath(oldpathstr);       % Remove the old one
+                        addpath(usePath, '-END'); % Add the new one
+                        addpath(si,'-END');       % Add the core si base after that.
+
                     else
-                        error('Units system folder %s does not apear to be valid.', usePath);
+
+                        rmpath(oldpathstr);       % Remove the old one
+                        addpath(si,'-END');       % Add the core si base.
+                    end
+
+                    if nargin > 2
+
+                        % unit class option
+
+                        [~] = units('class',varargin{3});
+                    end
+
+                    % If output desired, we return the previous base system path
+
+                    if nargout > 0
+                        varargout{1} = oldpathstr;
+                    else
+                        unit_SYSTEM
                     end
 
                 end
-
-                % Get si base bath
-                si = fullfile(fileparts(which(mfilename)), 'si');
                 
-                rmpath(oldpathstr);       % Remove the old one to avoid problems
-                addpath(usePath, '-END'); % Add the new one
-                addpath(si,'-END');       % Add the core si base after that.
-
-                % If output desired, we return the previous base system path
-                
-                if nargout > 0
-                    varargout{1} = oldpathstr;
-                end
-
             else % report current base system folder
                 
-                pathstr = fileparts(which('unit_SYSTEM'));
-
                 if nargout == 0
-                    fprintf('%s\n', pathstr);
+                    fprintf('%s\n', oldpathstr);
                 else
-                    varargout{1} = pathstr;
+                    varargout{1} = oldpathstr;
                 end
                 
             end
@@ -196,26 +229,67 @@ elseif ischar(varargin{1})
                 bases = {'MASS','LENGTH','TIME','TEMPERATURE', ...
                          'AMOUNT','LUMINANCE', 'CURRENT', ...
                          'ANGLE_PLANE','ANGLE_SOLID','INFORMATION'};
-
-                for i = 1:length(bases)
-
-                    val = feval(['unit_' bases{i}]);
-                    info = feval(['unit_' bases{i} '_info']);
-                    fprintf('%-12s: %s (%s) = %g\n', bases{i}, info.name, info.symbol, double(val));
-
-                end
-
             else
+                
+                bases = upper(varargin(2));
+            end
+            
+            for i = 1:length(bases)
+
+                val = feval(['unit_' bases{i}]);
+                info = feval(['unit_' bases{i} '_info']);
+                fprintf('%-12s: %s (%s) = %g\n', bases{i}, info.name, info.symbol, double(val));
+
+            end
+
+        case 'class'
+            
+            if nargin == 1
+                
+                if nargout == 0
+                    
+                    disp(class(unit_TIME));
+                else
+                    varargout{1} = class(unit_TIME);
+                end
+                
+            else
+                
+                dbl = fileparts(which(mfilename));
+                oldpathstr = fileparts(which('unit_MAKE'));
                 
                 switch lower(varargin{2})
                     
-                    case 'obj'
-            
-                        error('Will switch to unitval object based units in future');
+                    case 'double'
+
+                        rmpath(oldpathstr);
+                        addpath(dbl,'-END');
                         
-                    case {'double','normal'}
+                    case {'object','unitval'}
                         
-                        error('Will switch to non-object based units in future');
+                        if ~exist('unitval','class')
+                            error('unitval class not found');
+                        else
+                            fobj = fullfile(dbl,'siobj');
+                            mak  = fullfile(fobj,'unit_MAKE.m');
+                            if ~exist(fobj,'dir') || ~exist(mak,'file')
+                                error('Cannot find %s', mak);
+                            end
+                        end
+                        rmpath(oldpathstr);
+                        addpath(fobj, '-END');
+                        addpath(dbl,'-END');
+                        
+                    otherwise
+                        
+                        error('Unknown units class: %s', varargin{2});
+                        
+                end
+                
+                if nargout == 0
+                    unit_SYSTEM
+                else
+                    varargout{1} = class(unit_TIME);
                 end
             end
             
