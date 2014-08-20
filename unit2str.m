a217f1ddc8e0af3507c93426c415208aeb3d5d57
Ryan Gray <ryangray@comcast.net>
2014-08-20 20:07:01 +0000

    Change output to return a cell array of strings if the input array has more than one row. Detect and remove a trailing comma from the resulting strings before appending the units string so that you can use a comma in the format string to create a comma-separated list.

--- unit2str.m
+++ unit2str.m
@@ -7,15 +7,24 @@
 %  str = unit2str(val, unitstr)
 %  str = unit2str(val, unitstr, format)
 %
-% The number is converted to a string with num2str(). If format is
-% specified, then it is passed to num2str().
+% The number is converted to the units specified with unitstr and then to a
+% string with num2str(). If format is specified, then it is passed to
+% num2str().
 %
-% If val is an array, it works as num2str does: 1xN will produce a single
-% string of values with one unit at the end, and an Nx1 array will produce
-% a character array with N rows and the unit repeated at the end of each
-% row which you can then run through cellstr() or cellstr(strtrim()) if you
-% like as you might do with num2str().
+% If val is an array, a 1xN array will produce a single string of values
+% with one unit at the end, and an Nx1 array will produce a cell array of
+% strings with N rows and the unit repeated at the end of each row.
 %
+% You can give the units string with the units surrounded by parend or
+% brackets if you want them to appear after the value that way:
+%
+%  unit2str(x, '(km)')
+%
+% If you want multiple values to be separated by a comma, specify the
+% format with a comma trailing the value (the last one will be removed):
+%
+%  unit2str(x, '(km)', '%g, ')
+% 
 % See also: units, unitsSymbols
 
 function str = unit2str (value, unitstr, format)
@@ -39,6 +48,12 @@ else
     
 end
 
+if size(nstr,1) > 1
+    nstr = cellstr(nstr);
+end
+
+nstr = regexprep(nstr, ',?\s*$',''); % remove a trailing comma and spaces
+
 % Attach units to every row of values
 
-str = [nstr repmat(ustr, size(nstr,1), 1)];
+str = strcat(nstr, repmat(ustr, size(nstr,1), 1));
