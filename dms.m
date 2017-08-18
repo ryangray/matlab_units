dbaeec1507a49f9a50bb12028c0efd3fba3f73a4
Ryan Gray <ryan@graytrek.com>
2017-08-18 23:09:19 +0000

    dms.m still had many problems. Further fixed the sign handling by stripping off the sign and putting it back on the result because of the use of floor(). Also put sign on the m and s outputs, as well as handle the sign on the m and s inputs.
    git-svn-id: file:///m/SVN/units@176 4ff8c76e-cb3a-aa4d-a7c4-9fd09a33d28f

--- dms.m
+++ dms.m
@@ -1,48 +1,68 @@
-% Convert dms to or from seconds
-% Convert degrees, minutes, seconds of arc to a decimal angle in radians
-% Usage: r = dms(dd.mmss)                     % if m & s are encoded in the decimal
-%        r = dms(degrees, minutes)
-%        r = dms(degrees, minutes, seconds)
-% The first three forms are for angle values in degrees, not radians.
-% The following form is for converting a radians ange to DMS form.
-%        [d,m,s] = dms(r)   % return DMS components separately
+%% Convert angle to or from dms form
+% Usage: a = dms(dd.mmss)                     % Convert from DMS with m & s encoded in the decimal to radians.
+%        a = dms(degrees, minutes)            % Convert from degrees & minutes (0 seconds) to radians.
+%        a = dms(degrees, minutes, seconds)   % Convert from degrees, minutes and seconds to radians.
+% The first three forms are for the "d" angle values in degrees, not radians.
+% The following form is for converting a radians angle to DMS form.
+%        [d,m,s] = dms(a)   % return DMS components separately
+%        [d,m,s,D] = dms(a) % D output is in the d.mmss form.
+% Negative angles produce negative values for d, m, s, and D (unless some are
+% 0). For d, m, and s inputs, a negative value should have consistent sign
+% (not including zero values), but this is not checked.
+
 % Future extensions could be to accept string forms of DMS like:
 %   dd.mmss or dd°mm'ss"
 
 function [angle, varargout] = dms (d, varargin)
 
-if nargout == 3
+sgn = sign(d);
+sgn(sgn == 0) = 1;
+d = abs(d);
+
+if nargout >= 3
     
-    % Convert radian angle to d, m and s outputs
+    % Convert radian angle input d to degrees, minutes and seconds outputs
     
     dg = d / deg;
     angle = floor(dg);
     mn = 60 * (dg - angle);
     sc = 60 * (mn - floor(mn));
-    varargout = {floor(mn), sc};
+    mn = floor(mn);
+    angle = sgn * angle;
     
+    if nargout > 3
+        % Add d.mmss output
+        D = sgn * (angle + mn / 100 + sc / 10000);
+        varargout = {sgn * mn, sgn * sc, D};
+    else
+        varargout = {sgn * mn, sgn * sc};
+    end
     
 else
     
-    % Decode dd.mmss in first argument
-    
-    ms = d - floor(d);
-    mn = floor(ms * 100);
-    sc = (ms * 100 - mn) * 100;
-    
-    angle = floor(d) * degrees + mn / 60 * deg + sc / 3600 * deg;
-
     % If more than one input, then first was just dd and the other were mm,ss
     
     if ~isempty(varargin)
 
-        angle = angle + sign(d) * varargin{1} / 60 * deg;
+        mn = varargin{1};
+        angle = (sgn * d + mn / 60) * deg;
 
-    end
+        if length(varargin) > 1
 
-    if length(varargin) > 1
+            sc = varargin{2};
+            angle = angle + sc / 3600 * deg;
 
-        angle = angle + sign(d) * varargin{2} / 3600 * deg;
+        end
+
+    else
+        
+        % Decode dd.mmss in first argument
+
+        ms = d - floor(d);
+        mn = floor(ms * 100);
+        sc = (ms * 100 - mn) * 100;
+
+        angle = sgn * (floor(d) + mn / 60 + sc / 3600) * deg;
 
     end
 
