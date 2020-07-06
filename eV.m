bc6d280587a3336836d0a6f973f1863ebb7d3de3
Ryan Gray <ryan.gray@mtsi-va.com>
2020-07-06 16:44:56 -0600

    Update eV value for 2018.

--- eV.m
+++ eV.m
@@ -1,10 +1,10 @@
 %% Unit eV (electron Volt)
 %
-% Source: CODATA 2006 via NIST
-% Uncertainty = 0.000 000 040 x 10-19 J
+% Source: CODATA 2018 via <https://physics.nist.gov/cgi-bin/cuu/Value?evj NIST>
+% eV = 1.602 176 634 x 10-19 J (exact)
 %
-% See also: kev, joule
+% See also: keV, joule
 
-function eV = unit
+function e = eV
 
-eV = unit_MAKE(1.602176487e-19 * joule, 'electron_volt', 'eV');
+e = unit_MAKE(1.602176634e-19 * joule, 'electron_volt', 'eV');
