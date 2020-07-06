a671fbd88a227d19e666d6305a7d917c2aa49e44
Ryan Gray <ryan.gray@mtsi-va.com>
2020-07-06 16:45:16 -0600

    Simplify logic

--- days.m
+++ days.m
@@ -1,4 +1,4 @@
-%% Unit days (solar day)
+%% Unit days (mean solar day)
 %
 % Also, days(month) will give the number of days in the month.
 % Month is by an integer 1..12. To get the right number of days for
@@ -6,77 +6,45 @@
 %
 % See also: day
 
-function x = days(varargin)
+function x = days (varargin)
 
 if nargin == 0
     
     x = day; 
     
-elseif varargin{1} == 1
+else
     
-    x = 31;
+    switch varargin{1}
+        
+        case {1,3,5,7,8,10,12}
     
-elseif varargin{1} == 2
+            x = 31;
     
-    if nargin == 1
-        
-        x = 28;
-        
-    else
-        
-        year = varargin{2};
-        
-        if (mod(year,100) ~= 0 && mod(year,  4) == 0) || ...
-           (mod(year,100) == 0 && mod(year,400) == 0)
-       
-            x = 29;
-            
-        else
-            
-            x = 28;
-            
-        end
-        
+        case {4,6,9,11}
+    
+            x = 30;
+    
+        case 2
+    
+            if nargin == 1
+                
+                x = 28;
+                
+            else
+                
+                year = varargin{2};
+                
+                if (mod(year,100) ~= 0 && mod(year,  4) == 0) || ...
+                   (mod(year,100) == 0 && mod(year,400) == 0)
+               
+                    x = 29;
+                    
+                else
+                    
+                    x = 28;
+                    
+                end
+                
+            end
     end
-    
-elseif varargin{1} == 3
-    
-    x = 31;
-    
-elseif varargin{1} == 4
-    
-    x = 30;
-    
-elseif varargin{1} == 5
-    
-    x = 31;
-    
-elseif varargin{1} == 6
-    
-    x = 30;
-    
-elseif varargin{1} == 7
-    
-    x = 31;
-    
-elseif varargin{1} == 8
-    
-    x = 31;
-    
-elseif varargin{1} == 9
-    
-    x = 30;
-    
-elseif varargin{1} == 10
-    
-    x = 31;
-    
-elseif varargin{1} == 11
-    
-    x = 30;
-    
-elseif varargin{1} == 12
-    
-    x = 31;
-    
 end
